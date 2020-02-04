bb0d86f0163daea60ccff7492d560b4736175efc
Pertti Kroger <pertti@qvik.fi>
2020-02-04 11:16:13 +0200

    Support javascript dialogs in web view

--- SwedbankPaySDK/Classes/SwedbankPayWebViewController.swift
+++ SwedbankPaySDK/Classes/SwedbankPayWebViewController.swift
@@ -22,6 +22,8 @@ class SwedbankPayWebViewController : UIViewController {
     private let webView: WKWebView
     private var lastRootPage: (navigation: WKNavigation?, baseURL: URL?)?
     
+    private var onJavascriptDialogDismissed: (() -> Void)?
+    
     var isAtRoot: Bool {
         return lastRootPage != nil
     }
@@ -43,8 +45,75 @@ class SwedbankPayWebViewController : UIViewController {
     }
     
     func load(htmlString: String, baseURL: URL?) {
+        dismissJavascriptDialog()
         let navigation = webView.loadHTMLString(htmlString, baseURL: baseURL)
         lastRootPage = (navigation, baseURL)
+    }
+    
+    private func showJavascriptDialog(alert: UIAlertController, onDialogDismissed: @escaping () -> Void) {
+        dismissJavascriptDialog()
+        self.onJavascriptDialogDismissed = onDialogDismissed
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func dismissJavascriptDialog() {
+        onJavascriptDialogDismissed?()
+        onJavascriptDialogDismissed = nil
+        presentedViewController?.dismiss(animated: false, completion: nil)
+    }
+}
+
+private extension SwedbankPayWebViewController {
+    enum JavascriptDialog {
+        case alert(() -> Void)
+        case confirm((Bool) -> Void)
+        case prompt((String?) -> Void, defaultValue: String?)
+        
+        func addTextField(alert: UIAlertController) {
+            if case .prompt(_, let defaultValue) = self {
+                alert.addTextField {
+                    $0.text = defaultValue
+                }
+            }
+        }
+        
+        var cancelHandler: (() -> Void)? {
+            switch self {
+            case .alert: return nil
+            case .confirm(let handler): return { handler(false) }
+            case .prompt(let handler, _): return { handler(nil) }
+            }
+        }
+        
+        func getOKHandler(alert: UIAlertController) -> () -> Void {
+            switch self {
+            case .alert(let handler): return handler
+            case .confirm(let handler): return { handler(true) }
+            case .prompt(let handler, _): return { handler(alert.textFields?.first?.text ?? "") }
+            }
+        }
+    }
+    
+    func show(javascriptDialog: JavascriptDialog, message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        javascriptDialog.addTextField(alert: alert)
+        
+        let okHandler = javascriptDialog.getOKHandler(alert: alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+            self.onJavascriptDialogDismissed = nil
+            okHandler()
+        })
+        
+        let cancelHandler = javascriptDialog.cancelHandler
+        if let cancelHandler = cancelHandler {
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in
+                self.onJavascriptDialogDismissed = nil
+                cancelHandler()
+            })
+        }
+        
+        showJavascriptDialog(alert: alert, onDialogDismissed: cancelHandler ?? okHandler)
     }
 }
 
@@ -152,4 +221,17 @@ extension SwedbankPayWebViewController : WKUIDelegate {
     func webViewDidClose(_ webView: WKWebView) {
         delegate?.remove(webViewController: self)
     }
+    
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        show(javascriptDialog: .alert(completionHandler), message: message)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        show(javascriptDialog: .confirm(completionHandler), message: message)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        show(javascriptDialog: .prompt(completionHandler, defaultValue: defaultText), message: prompt)
+    }
 }
+
