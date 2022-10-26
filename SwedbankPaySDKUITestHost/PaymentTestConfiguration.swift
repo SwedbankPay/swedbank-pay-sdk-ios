2933662342a2a6ec4a9bfb80662867fb6c0d9a1e
Olof Thorén <olof.thoren@qvik.fi>
2022-10-26 17:38:26 +0200

    Allowing tests for a non-MerchantBackendConfiguration

--- SwedbankPaySDKUITestHost/PaymentTestConfiguration.swift
+++ SwedbankPaySDKUITestHost/PaymentTestConfiguration.swift
@@ -23,6 +23,7 @@ let paymentTestConfigurationEnterprise = SwedbankPaySDK.MerchantBackendConfigura
 enum TestConfigurations: String {
     case enterprise
     case paymentsOnly
+    case nonMerchantConfig
 }
 var activeConfig: TestConfigurations = .paymentsOnly
 
@@ -45,3 +46,53 @@ let testPaymentOrderCheckin = SwedbankPaySDK.PaymentOrder(
     urls: .init(configuration: currentConfig, language: .English),
     payer: .init(requireConsumerInfo: false, digitalProducts: false, shippingAddressRestrictedToCountryCodes: ["NO"], payerReference: "test payer")
 )
+
+// MARK: Allowing tests for a non-MerchantBackendConfiguration
+
+let testNonMerchantPaymentOrder = SwedbankPaySDK.PaymentOrder(
+    currency: "SEK",
+    amount: 200,
+    vatAmount: 50,
+    description: "Test Purchase",
+    urls: .init(hostUrls: [Constants.webViewaseUrl], completeUrl: Constants.completeUrl, cancelUrl: Constants.cancelUrl, paymentUrl: Constants.paymentUrl)
+)
+
+enum SwedbankPayConfigurationError: Error {
+    case notImplemented
+}
+struct Constants {
+    static let webViewaseUrl = URL(string: "https://www.nonMerchantConfig.se")!
+    static let completeUrl = URL(string: "nonMerchantConfig-test://complete-payment")!
+    static let cancelUrl = URL(string: "nonMerchantConfig-test://cancel-payment")
+    static let paymentUrl = URL(string: "nonMerchantConfig-test://payment")
+}
+
+class SwedbankPayConfiguration {
+    let orderInfo: SwedbankPaySDK.ViewPaymentOrderInfo
+    
+    init(viewPaymentLink: URL) {
+        self.orderInfo = SwedbankPaySDK.ViewPaymentOrderInfo(
+            isV3: true,
+            webViewBaseURL: Constants.webViewaseUrl,
+            viewPaymentLink: viewPaymentLink,
+            completeUrl: Constants.completeUrl,
+            cancelUrl: Constants.cancelUrl,
+            paymentUrl: Constants.paymentUrl,
+            termsOfServiceUrl: nil
+        )
+    }
+}
+
+
+extension SwedbankPayConfiguration: SwedbankPaySDKConfiguration {
+    
+    // This delegate method is not used but required
+    func postConsumers(consumer: SwedbankPaySDK.Consumer?, userData: Any?, completion: @escaping (Result<SwedbankPaySDK.ViewConsumerIdentificationInfo, Error>) -> Void) {
+        completion(.failure(SwedbankPayConfigurationError.notImplemented))
+    }
+    
+    func postPaymentorders(paymentOrder: SwedbankPaySDK.PaymentOrder?, userData: Any?, consumerProfileRef: String?, options: SwedbankPaySDK.VersionOptions, completion: @escaping (Result<SwedbankPaySDK.ViewPaymentOrderInfo, Error>) -> Void) {
+        completion(.success(orderInfo))
+    }
+    
+}
