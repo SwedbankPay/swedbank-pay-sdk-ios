62d16a41f92d1f0eae4bc560a5d1a558b80cfe4e
Michael Balsiger <michael@appcorn.se>
2024-05-13 15:05:36 +0200

    SP-8 REST API klient

---
+++ SwedbankPaySDK/Classes/Api/SwedbankPayAPIError.swift
@@ -0,0 +1,32 @@
+//
+// Copyright 2024 Swedbank AB
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+
+enum SwedbankPayAPIError: Error {
+    case invalidUrl
+    case unknown
+}
+
+extension SwedbankPayAPIError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidUrl:
+            return SwedbankPaySDKResources.localizedString(key: "swedbankpaysdk_native_invalid_url")
+        case .unknown:
+            return SwedbankPaySDKResources.localizedString(key: "swedbankpaysdk_native_unknown")
+        }
+    }
+}
